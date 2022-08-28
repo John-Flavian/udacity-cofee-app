67f8736b349a269a73ef1713007fd6ae76c3d435
John Flavian <flavianomeokwe@gmail.com>
2022-08-29 00:58:15 +0100

    modified the necessary files.

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'fsmg.us.auth0.com', // the auth0 domain prefix
+    url: 'fsmg.us', // the auth0 domain prefix
     audience: 'dev', // the audience set for the auth0 app
     clientId: 'BT08g1slPWIKQ8gWQqXHpunQmFx9k8yD', // the client id generated for the auth0 app
     callbackURL: 'https://localhost:8100', // the base url of the running ionic application. 
